08544ab1bf53dc83295bd81f856e42af1151ff13
Indra Wijaya <indraakmal08@gmail.com>
2025-05-30 16:25:04 +0800

    Add package cors

--- src/application/web.js
+++ src/application/web.js
@@ -1,9 +1,18 @@
 import express from "express";
-import {publicRouter} from "../route/public-api.js";
-import {errorMiddleware} from "../middleware/error-middleware.js";
-import {userRouter} from "../route/api.js";
+import { publicRouter } from "../route/public-api.js";
+import { errorMiddleware } from "../middleware/error-middleware.js";
+import { userRouter } from "../route/api.js";
+import cors from "cors";
 
 export const web = express();
+
+web.use(
+  cors({
+    origin: ["http://localhost:3000", "http://localhost:5173"],
+    credentials: true,
+  })
+);
+
 web.use(express.json());
 
 web.use(publicRouter);
